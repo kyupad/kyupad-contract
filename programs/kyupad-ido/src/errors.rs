49d0f1f7609b12317065f3e8b69ec009e43fd31d
Chau Khac <chaukhac4@gmail.com>
2024-05-10 18:41:29 +0700

    feat: Delete unused code

--- programs/kyupad-ido/src/errors.rs
+++ programs/kyupad-ido/src/errors.rs
@@ -2,50 +2,15 @@ use anchor_lang::error_code;
 
 #[error_code]
 pub enum KyuPadError {
-    #[msg("Could not save guard to account")]
-    InvalidAccountSize,
-
     #[msg("This wallet is't supported to mint")]
     InvalidWallet,
-
-    #[msg("The merkle root is invalid")]
-    InvalidMekleRoot,
-
-    #[msg("Pool's supply has run out")]
-    PoolSupplyRunOut,
-
-    #[msg("Not enough sol to mint")]
-    NotEnoughSOL,
 
     PublicKeyMismatch,
 
     IncorrectOwner,
 
-    AllowedMintLimitReached,
-
     #[msg("Mint time is too early or expired")]
     NotInvestTime,
-
-    #[msg("This pool config is already in pools")]
-    CannotAddPoolConfig,
-
-    #[msg("This destination address doesn't not match with pools config")]
-    DestinationIsInvalid,
-
-    #[msg("This signer is now allow to init another signer")]
-    InvalidSigner,
-
-    #[msg("Error unknown")]
-    ErrorUnknown,
-
-    #[msg("Your pool id doesn't in pools config")]
-    PoolIdInvalid,
-
-    #[msg("Invalid merkle root")]
-    InvalidMerkeRoot,
-
-    #[msg("Dont have right to do this action")]
-    DontHaveRight,
 
     #[msg("Projet is out of tickets to buy")]
     ProjectOutOfTicket,
